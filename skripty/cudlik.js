a7fa5a3320795d60a7239944e281c60ea74d8d99
jarabum <101054479+jarabum@users.noreply.github.com>
2024-08-29 16:11:53 +0200

    přidané souřadnice na kupony

--- skripty/cudlik.js
+++ skripty/cudlik.js
@@ -1,6 +1,6 @@
 function zmacknuto() {
     document.querySelector(".button").
     onclick = function () {
-    alert("souradnice");
+    alert("NAJDI BÁJNÉ KUPONY NA CHLEBÍK NA TĚCHTO SOUŘADNICÍCH 228 64 -804");
     }
 }
